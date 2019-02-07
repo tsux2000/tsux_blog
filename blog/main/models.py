cde6e6bfb27a7db418342858b723a8cfbea87b32
tsux2000 <tsux2000@gmail.com>
2019-02-07 20:01:41 +0900

    Fourth commit

--- blog/main/models.py
+++ blog/main/models.py
@@ -1,70 +1,76 @@
 from django.db import models
+from django.utils import timezone
 
 class User(models.Model):
-    user_id = models.Field()
-    status_id = models.Field()
-    position_id = models.Field()
-    slug = models.Field()
-    first_name = models.Field()
-    middle_name = models.Field()
-    last_name = models.Field()
-    disp_name = models.Field()
-    password = models.Field()
-    birth_date = models.Field()
-    phone = models.Field()
-    bio = models.Field()
-    create_date = models.Field()
-    update_date = models.Field()
-    user_img_id = models.Field()
+    user_id = models.IntegerField(unique = True,)
+    status_id = models.IntegerField()
+    position_id = models.IntegerField(blank = True,)
+    slug = models.SlugField(unique = True,)
+    first_name = models.CharField(max_length = 255,)
+    last_name = models.CharField(max_length = 255, blank = True,)
+    disp_name = models.CharField(max_length = 255, unique = True,)
+    password = models.CharField(max_length = 255,)
+    birth_date = models.DateField(blank = True,)
+    phone = models.SlugField(blank = True,)
+    email = models.EmailField()
+    bio = models.TextField(blank = True,)
+    create_date = models.DateField(default = timezone.now,)
+    update_date = models.DateField(auto_now = True,)
+    user_img = models.SlugField(blank = True,)
 
-    def __str__(self):
-        return self.id + "_" + self_slug
+    # class Meta:
+    #     abstract = True
 
 class AdminUser(User):
-    admin_user_id = user_id
+    admin_user_id = User.user_id
 
 class CommonUser(User):
-    common_user_id = user_id
+    common_user_id = User.user_id
 
 class Post(models.Model):
-    post_id = models.Field()
-    slug = models.Field()
-    category_id = models.Field()
-    subcategory_id = models.Field()
-    author_id = models.Field()
-    status_id = models.Field()
-    tag_id = models.Field()
-    title = models.Field()
-    description = models.Field()
-    keywords = models.Field()
-    contents = models.Field()
-    thumbnail = models.Field()
-    create_date = models.Field()
-    update_date = models.Field()
+    post_id = models.IntegerField(unique = True,)
+    slug = models.SlugField(unique = True,)
+    category_id = models.IntegerField()
+    subcategory_id = models.IntegerField(blank = True,)
+    author_id = models.IntegerField()
+    status_id = models.IntegerField()
+    title = models.CharField(max_length = 255, unique = True,)
+    description = models.TextField(blank = True,)
+    keywords = models.CharField(max_length = 255, blank = True,)
+    contents = models.TextField(unique = True,)
+    thumbnail = models.SlugField(blank = True,)
+    create_date = models.DateField(default = timezone.now)
+    update_date = models.DateField(auto_now = True,)
 
 class Comment(models.Model):
-    comment_id = models.Field()
-    title = models.Field()
-    user_id = models.Field()
-    content = models.Field()
-    post_id = models.Field()
-    status_id = models.Field()
-    create_date = models.Field()
+    comment_id = models.IntegerField(unique = True,)
+    title = models.CharField(max_length = 255, blank = True,)
+    user_id = models.IntegerField()
+    contents = models.TextField()
+    post_id = models.IntegerField(blank = True,)
+    status_id = models.IntegerField()
+    create_date = models.DateField(default = timezone.now)
 
 class Attr(models.Model):
-    attr_id = models.Field()
-    slug = models.Field()
-    name = models.Field()
-    status_id = models.Field()
+    attr_id = models.IntegerField(unique = True,)
+    slug = models.SlugField(unique = True,)
+    name = models.CharField(max_length = 255, unique = True,)
+    status_id = models.IntegerField()
+
+    class Meta:
+        abstract = True
 
 class Category(Attr):
-    category_id = attr_id
+    category_id = Attr.attr_id
 
 class SubCategory(Attr):
-    sub_category_id = attr_id
+    sub_category_id = Attr.attr_id
 
 class Status(Attr):
-    status_id = attr_id
+    status_id = Attr.attr_id
+
+class Tag(Attr):
+    tag_id = Attr.attr_id
 
 class Position(Attr):
-    position_id = attr_id
+    position_id = Attr.attr_id
--- blog/main/urls.py
+++ blog/main/urls.py
@@ -3,4 +3,19 @@ from .views import MainView
 
 urlpatterns = [
     path('', MainView.as_view(), name = 'index'),
+    path('search/', MainView.as_view(), name = 'list'),
+    path('<post_slug>/', MainView.as_view(), name = 'post'),
+    path('archive/<year>/', MainView.as_view(), name = 'list'),
+    path('archive/<year>/<month>/', MainView.as_view(), name = 'list'),
+    path('category/<category_slug>/', MainView.as_view(), name = 'list'),
+    path('category/<category_slug>/subcategory/<subcategory_slug>/', MainView.as_view(), name = 'list'),
+    path('author/', MainView.as_view(), name = 'user_list'),
+    path('author/<author_slug>/', MainView.as_view(), name = 'index'),
+    path('about_blog_q/', MainView.as_view(), name = 'index'),
+    path('sitemap/', MainView.as_view(), name = 'index'),
+    path('policy/', MainView.as_view(), name = 'index'),
+    path('log_in/', MainView.as_view(), name = 'index'),
+    path('sign_up/', MainView.as_view(), name = 'index'),
+    path('my_page/<user_id>/', MainView.as_view(), name = 'index'),
+    path('about_us/', MainView.as_view(), name = 'index'),
 ]
